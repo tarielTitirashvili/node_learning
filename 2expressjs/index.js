cbd38eb92c363697c370d3bf5ebf5c1a66a59132
tarielTitirashvili <tarieltitirashvili@gmail.com>
2026-05-06 19:44:00 +0400

    css files for html public folder with statically served files

--- 2expressjs/index.js
+++ 2expressjs/index.js
@@ -1,13 +1,16 @@
 // 3rd party
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 // my code
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const notFoundRouter = require('./routes/notFound')
+const rootDir = require('./util/path')
 
 const app = express()
 
+app.use(express.static(path.join(rootDir, 'public')))
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/admin',adminRoutes)
